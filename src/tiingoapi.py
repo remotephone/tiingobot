f1565b9ab4b82c465acd6073a791664c8429f211
remotephone <remotephone@gmail.com>
2021-03-24 22:15:11 -0500

    major reformatting, adding logging all around, added error checking

--- src/tiingoapi.py
+++ src/tiingoapi.py
@@ -1,109 +1,174 @@
 import json
+import logging
 import os
 import re
 from datetime import datetime, timedelta
 
 import requests
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fhandler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+fhandler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+shandler = logging.StreamHandler()
+shandler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(fhandler)
+logger.addHandler(shandler)
+
 
 def validate_stonk(stock):
-    print(stock)
-    if re.search(r'^[A-Za-z][\S]{0,4}$', stock):
+    if re.search(r"^[A-Za-z][\S]{0,4}$", stock):
+        logger.info(f"validated {stock}")
         return stock
     else:
+        logger.info(f"Failed to validate {str(stock)}")
         return "gme"
 
+
 def is_new(time):
+    """Had the logic backwards here at first. If the test passes and the dates for the
+    quote and now are less than 3 days apart, assume the stock is currently listed and
+    keep it in the list by returning true. Otherwise return false and remove it."""
     dtobj = datetime.fromisoformat(time)
     tz_info = dtobj.tzinfo
-    if (datetime.now(tz_info) - dtobj) > timedelta(days=1): 
-        return False
-    else:
+    if (datetime.now(tz_info) - dtobj) < timedelta(days=3):
         return True
+    else:
+        return False
+
 
 def get_stocks(stock):
-    TOKEN = os.environ['TIINGO_TOKEN']
-    stock = validate_stonk(stock)
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    response = requests.get(f"https://api.tiingo.com/iex/?tickers={stock}&token={TOKEN}", headers=headers)
+    """ return a stock quote, cleaned up """
+    TOKEN = os.environ["TIINGO_TOKEN"]
+    validstock = validate_stonk(stock)
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.get(
+            f"https://api.tiingo.com/iex/?tickers={validstock}&token={TOKEN}",
+            headers=headers,
+        )
+        validstock = response.json()
+    except Exception as e:
+        logger.info(f"Failed to connect to tiingo api. Reason: {e}")
+        validstock = []
     # This returns a list of dictionaries with each item a stock
     # [{'askPrice': None, 'ticker': 'AAPL', 'mid': None, 'quoteTimestamp': '2021-03-15T20:00:00+00:00', 'timestamp': '2021-03-15T20:00:00+00:00', 'askSize': None, 'open': 121.41, 'prevClose': 121.03, 'tngoLast': 123.99, 'bidSize': None, 'lastSaleTimestamp': '2021-03-15T20:00:00+00:00', 'volume': 92590555, 'bidPrice': None, 'low': 120.42, 'lastSize': None, 'high': 124.0, 'last': 123.99}]
-    stock = response.json()
 
     clean_stock = {}
 
-    if stock == []:
-        clean_stock['Result'] =  "Not Found, use a good ticker"
+    if validstock == []:
+        clean_stock["Result"] = "Not Found, use a good ticker"
+        logger.info(f"ticker failed to return any results for {str(stock)}")
     else:
-        clean_stock['Ticker'] = stock[0]['ticker']
-        clean_stock['Quote Timestamp'] = stock[0]['quoteTimestamp']
-        clean_stock['Most Recent Price'] = stock[0]['last']
-        clean_stock['Open'] = stock[0]['open']
-        clean_stock['High'] = stock[0]['high']
-        clean_stock['Low'] = stock[0]['low']
-        clean_stock['% Change'] = f"{round(((stock[0]['last'] - stock[0]['open']) / stock[0]['open']) * 100, 2)}%" 
-        if stock[0]['last'] > stock[0]['open']:
-            clean_stock['Mood'] = "\U0001F4C8"
-        elif stock[0]['last'] < stock[0]['open']:
-            clean_stock['Mood'] = "\U0001F4C9"
+        clean_stock["Ticker"] = validstock[0]["ticker"]
+        clean_stock["Quote Timestamp"] = validstock[0]["quoteTimestamp"]
+        clean_stock["Most Recent Price"] = validstock[0]["last"]
+        clean_stock["Open"] = validstock[0]["open"]
+        clean_stock["High"] = validstock[0]["high"]
+        clean_stock["Low"] = validstock[0]["low"]
+        clean_stock[
+            "% Change"
+        ] = f"{round(((stvalidstockock[0]['last'] - validstock[0]['open']) / validstock[0]['open']) * 100, 2)}%"
+        if stovalidstockck[0]["last"] > validstock[0]["open"]:
+            clean_stock["Mood"] = "\U0001F4C8"
+        elif validstock[0]["last"] < validstock[0]["open"]:
+            clean_stock["Mood"] = "\U0001F4C9"
 
     return clean_stock
 
-def get_em_all():
-    TOKEN = os.environ['TIINGO_TOKEN']
 
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    response = requests.get(f"https://api.tiingo.com/iex/?token={TOKEN}", headers=headers)
-    stocks = response.json()
-    return stocks
+def get_em_all():
+    """a helper function for stonkest and stankest. Get me all the stocks
+    and return them as a dictionary for further processing"""
+    TOKEN = os.environ["TIINGO_TOKEN"]
+
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.get(
+            f"https://api.tiingo.com/iex/?token={TOKEN}", headers=headers
+        )
+        stocks = response.json()
+        return stocks
+    except Exception as e:
+        logger.info(f"error: {e}")
+        stocks = []
+        return stocks
+
 
 def get_stonkest():
     stocks = get_em_all()
-
+    if len(stocks) == 0:
+        return None
     clean_stocks = []
 
     for stock in stocks:
         clean_stock = {}
-        clean_stock['Ticker'] = stock['ticker']
-        clean_stock['Quote Timestamp'] = stock['quoteTimestamp']
-        clean_stock['Most Recent Price'] = stock['last']
-        clean_stock['Open'] = stock['open']
-        clean_stock['\U0001F680'] = round(((float(stock['last']) - float(stock['open'])) / float(stock['open'])) * 100, 2) 
+        clean_stock["Ticker"] = stock["ticker"]
+        clean_stock["Quote Timestamp"] = stock["quoteTimestamp"]
+        clean_stock["Most Recent Price"] = stock["last"]
+        clean_stock["Open"] = stock["open"]
+        clean_stock["\U0001F680"] = round(
+            ((float(stock["last"]) - float(stock["open"])) / float(stock["open"]))
+            * 100,
+            2,
+        )
         clean_stocks.append(clean_stock)
 
-    no_oldies = [clean_stock for clean_stock in clean_stocks if (is_new(clean_stock['Quote Timestamp']))] 
-    no_pennies = [clean_stock for clean_stock in no_oldies if (clean_stock['Most Recent Price'] > 1.0)] 
-    stonkest = sorted(no_pennies, key = lambda x: x['\U0001F680'])
+    no_oldies = [
+        clean_stock
+        for clean_stock in clean_stocks
+        if (is_new(clean_stock["Quote Timestamp"]))
+    ]
+    no_pennies = [
+        clean_stock
+        for clean_stock in no_oldies
+        if (clean_stock["Most Recent Price"] > 1.0)
+    ]
+    stonkest = sorted(no_pennies, key=lambda x: x["\U0001F680"])
 
     for stonk in stonkest:
-        stonk['\U0001F680'] = "{}% up up up".format(str(stonk['\U0001F680']))
+        stonk["\U0001F680"] = "{}% up up up".format(str(stonk["\U0001F680"]))
 
     return stonkest[-5:]
 
 
 def get_stankest():
     stocks = get_em_all()
+    if len(stocks) == 0:
+        return None
 
     clean_stocks = []
 
     for stock in stocks:
         clean_stock = {}
-        clean_stock['Ticker'] = stock['ticker']
-        clean_stock['Quote Timestamp'] = stock['quoteTimestamp']
-        clean_stock['Most Recent Price'] = stock['last']
-        clean_stock['Open'] = stock['open']
-        clean_stock['\U0001F4A5'] = round(((float(stock['last']) - float(stock['open'])) / float(stock['open'])) * 100, 2) 
+        clean_stock["Ticker"] = stock["ticker"]
+        clean_stock["Quote Timestamp"] = stock["quoteTimestamp"]
+        clean_stock["Most Recent Price"] = stock["last"]
+        clean_stock["Open"] = stock["open"]
+        clean_stock["\U0001F4A5"] = round(
+            ((float(stock["last"]) - float(stock["open"])) / float(stock["open"]))
+            * 100,
+            2,
+        )
         clean_stocks.append(clean_stock)
 
-    no_oldies = [clean_stock for clean_stock in clean_stocks if (is_new(clean_stock['Quote Timestamp']))] 
-    no_pennies = [clean_stock for clean_stock in no_oldies if (clean_stock['Most Recent Price'] > 1.0)] 
-    stankest = sorted(no_pennies, key = lambda x: x['\U0001F4A5'], reverse=True)
+    no_oldies = [
+        clean_stock
+        for clean_stock in clean_stocks
+        if (is_new(clean_stock["Quote Timestamp"]))
+    ]
+    no_pennies = [
+        clean_stock
+        for clean_stock in no_oldies
+        if (clean_stock["Most Recent Price"] > 1.0)
+    ]
+    stankest = sorted(no_pennies, key=lambda x: x["\U0001F4A5"], reverse=True)
 
     for stonk in stankest:
-        stonk['\U0001F4A5'] = "{}% down down down".format(str(stonk['\U0001F4A5']))
+        stonk["\U0001F4A5"] = "{}% down down down".format(str(stonk["\U0001F4A5"]))
 
     return stankest[-5:]
--- src/tiingobot.py
+++ src/tiingobot.py
@@ -3,29 +3,41 @@ import os
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
-from tiingoapi import get_stocks, get_stonkest, get_stankest
+from tiingoapi import get_stankest, get_stocks, get_stonkest
 
-logging.basicConfig(filename='tiingo.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fhandler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+fhandler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+shandler = logging.StreamHandler()
+shandler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(fhandler)
+logger.addHandler(shandler)
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-bot = discord.ext.commands.Bot(command_prefix = "!")
 
 @bot.event
 async def on_ready():
-    logging.info(f'tiingobot has connected to Discord!')
+    logger.info(f"tiingobot has connected to Discord!")
 
 
 @bot.event
 async def on_command_error(error, ctx):
     if isinstance(error, commands.CommandOnCooldown):
-        await bot.send_message(ctx.message.channel, content=f"This command is on a {error.retry_after} cooldown" )
-    logging.info(error)  # re-raise the error so all the errors will still show up in console
+        await bot.send_message(
+            ctx.message.channel,
+            content=f"This command is on a {error.retry_after} cooldown",
+        )
+    logger.info(
+        error
+    )  # re-raise the error so all the errors will still show up in console
 
 
-@bot.command(name='stonkshelp', help='return help')
+@bot.command(name="stonkshelp", help="return help")
 async def stonks(ctx):
     helpmsg = """Gimme a stonk ticker, I only accept 4 character symbols. \n10 second cooldown enforced, and no funny business.\n \
         Supported commands:\n\
@@ -34,38 +46,56 @@ async def stonks(ctx):
         !stonkest - gimme the stonkingest stonks of the day (most positive % change)\n\
         !stankest - gimme the stankingest stonks of the day (most negative % change)\n\
             (These last two omit stocks worth less than $1)"""
+    logger.info(f"{ctx.author} requested help")
     await ctx.send(helpmsg)
 
 
-@bot.command(name='stonks', help='Return stock message, defaults to GME if trickery is afoot')
+@bot.command(
+    name="stonks", help="Return stock message, defaults to GME if trickery is afoot"
+)
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def stonks(ctx, stock: str):
     ticker = get_stocks(stock)
     ticker_response = ""
     for k, v in ticker.items():
         ticker_response += k + ": " + str(v) + "\n"
+    logger.info(f"{ctx.author} requested stock {stock}")
     await ctx.send(ticker_response)
 
 
-@bot.command(name='stonkest', help='Return top 5 most performat stocks by percent')
+@bot.command(name="stonkest", help="Return top 5 most performat stocks by percent")
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def stonkest(ctx):
     stonkest = get_stonkest()
+    if len(stonkest) == 0:
+        logger.info(f"{ctx.author} requested stankest. No results returned")
+        await ctx.send("Something went wrong")
     stonkest_response = ""
+    tickers = []
     for stonk in stonkest:
         for k, v in stonk.items():
             stonkest_response += k + ": " + str(v) + "\n"
+            if k == "Ticker":
+                tickers.append(v)
+    logger.info(f"{ctx.author} requested stonkest. returned tickers: {tickers}")
     await ctx.send(stonkest_response)
 
 
-@bot.command(name='stankest', help='Return bottom 5 most performat stocks by percent')
+@bot.command(name="stankest", help="Return bottom 5 most performat stocks by percent")
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def stankest(ctx):
     stankest = get_stankest()
+    if len(stankest) == 0:
+        logger.info(f"{ctx.author} requested stankest. No results returned")
+        await ctx.send("Something went wrong")
     stankest_response = ""
     for stonk in stankest:
         for k, v in stonk.items():
             stankest_response += k + ": " + str(v) + "\n"
+            if k == "Ticker":
+                tickers.append(v)
+    logger.info(f"{ctx.author} requested stankest. returned tickers: {tickers}")
     await ctx.send(stankest_response)
+
 
 bot.run(TOKEN)
