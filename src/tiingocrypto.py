5922edba60de3273a6f05c97faedd63f940670a1
remotephone <remotephone@gmail.com>
2021-12-07 23:22:23 -0600

    fixing url lol

--- src/tiingocrypto.py
+++ src/tiingocrypto.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import re
@@ -64,7 +65,7 @@ def get_crypto(crypto):
     headers = {"Content-Type": "application/json"}
     try:
         response = requests.get(
-            f"https://api.tiingo.com/tiingo/crypto/?tickers={validcrypto}&token={TOKEN}",
+            f"https://api.tiingo.com/tiingo/crypto/top?tickers={validcrypto}&token={TOKEN}",
             headers=headers,
         )
         validcrypto = response.json()
