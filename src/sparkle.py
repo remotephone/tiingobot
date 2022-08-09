0721b9bdeedc1df77ba382363ad400d86deeab00
remotephone <remotephone@gmail.com>
2022-08-08 21:55:20 -0500

    fixing requirements

--- src/sparkle.py
+++ src/sparkle.py
@@ -11,6 +11,8 @@ def db_connection():
     client = CosmosClient(os.environ['COSMOS_URI'], credential=os.environ['COSMOS_KEY'])
     database = client.get_database_client(os.environ['DATABASE_NAME'])
     container = database.get_container_client(os.environ['CONTAINER_NAME'])
+    return container
+
 
 # giver is not a string, not sure what it is
 def give_sparkle(giver: str, receiver: str) -> str:
@@ -30,20 +32,20 @@ def give_sparkle(giver: str, receiver: str) -> str:
         logger.error(f"Error inserting sparkle - {e}")
 
     for item in container.query_items(
-        query=f'SELECT VALUE COUNT(1) FROM sparkledb WHERE sparkledb.receiver = "{receiver}"',
-        enable_cross_partition_query=True):
+            query=f'SELECT VALUE COUNT(1) FROM sparkledb WHERE sparkledb.receiver = "{receiver}"',
+            enable_cross_partition_query=True):
         sparkle_count = str(item)
         logger.info(f"Got {sparkle_count} sparkles for {receiver}")
     return f"✨✨ {receiver} has {sparkle_count} sparkles! ✨✨"
+
 
 def get_leaderboard() -> str:
     container = db_connection()
 
     response = "✨✨Sparkle Leaderboard✨✨\n"
     for item in container.query_items(
-        query=f'SELECT TOP 3 COUNT(1) AS Sparkles, c.receiver AS Sparkled FROM sparkledb c GROUP BY c.receiver',
-        enable_cross_partition_query=True):
+            query='SELECT TOP 3 COUNT(1) AS Sparkles, c.receiver AS Sparkled FROM sparkledb c GROUP BY c.receiver',
+            enable_cross_partition_query=True):
         response += f"{item.get('Sparkled', 'Everyone Else')} has ✨✨{item.get('Sparkles', '0')}✨✨\n"
         logger.info(f"returning {response}")
     return response
-
