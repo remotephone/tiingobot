e3de358bbd27e4820f635e33c30487ecad16d7b7
remotephone <remotephone@gmail.com>
2023-09-23 16:29:44 -0500

    json loggin

--- src/tiingobot.py
+++ src/tiingobot.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import re
+import json
 
 import discord
 from discord.ext import commands
@@ -25,22 +25,39 @@ from tiingocrypto import get_crypto
 from sparkle import give_sparkle, get_leaderboard
 
 
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_data = {
+            'timestamp': self.formatTime(record),
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'module': record.module,
+            'function': record.funcName,
+            'line_number': record.lineno,
+        }
+
+        # Include additional data from the log record, if available
+        if hasattr(record, 'extra_data') and isinstance(record.extra_data, dict):
+            log_data.update(record.extra_data)
+
+        return json.dumps(log_data, ensure_ascii=False)
+
+
 logger = logging.getLogger("tiingobot_logger")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
+
+
+# Create a file handler
 fhandler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-fhandler.setFormatter(
-    logging.Formatter(
-        "%(asctime)s:%(levelname)s:%(name)s: {%(pathname)s:%(lineno)d}: %(message)s"
-    )
-)
-fhandler.setLevel(logging.ERROR)
+fhandler.setFormatter(JsonFormatter())
+fhandler.setLevel(logging.DEBUG)
+
+# Create a stream handler
 shandler = logging.StreamHandler()
-shandler.setFormatter(
-    logging.Formatter(
-        "%(asctime)s:%(levelname)s:%(name)s: {%(pathname)s:%(lineno)d}: %(message)s"
-    )
-)
-shandler.setLevel(logging.INFO)
+shandler.setFormatter(JsonFormatter())
+shandler.setLevel(logging.DEBUG)
+
+# Add both handlers to the logger
 logger.addHandler(fhandler)
 logger.addHandler(shandler)
 
