f57b28caa49a734ca8e8e57a48dc849841954cbf
remotephone <remotephone@gmail.com>
2023-10-06 09:39:02 -0500

    fixes and updates

--- src/tiingocrypto.py
+++ src/tiingocrypto.py
@@ -9,46 +9,36 @@ from dateutil.parser import parse
 
 logger = logging.getLogger("tiingobot_logger")
 
-
 def validate_stonk(crypto):
-    if re.search(r"^[A-Za-z0-9][\S]{0,15}$", crypto):
-        logger.info(f"validated {crypto}")
+    """Validate a crypto ticker."""
+    if crypto and re.match(r"^\w{1,16}$", crypto):
+        logger.info(f"Validated {crypto}")
         return crypto
     else:
-        logger.info(f"Failed to validate {str(crypto)}")
+        logger.info(f"Failed to validate {crypto}")
         return "btcusd"
 
 
 def is_new(time):
-    """Had the logic backwards here at first. If the test passes and the dates for the
-    quote and now are less than 3 days apart, assume the crypto is currently listed and
-    keep it in the list by returning true. Otherwise return false and remove it.
-    Also had to switch to dateutil.parser.parse because occassionally the timestamps
-    come back in a format that python doesnt recognize as iso and it throws an error.
-    Logging that now so I can troubleshoot it better and just returning True since it
-    only seems to do that with timestamps during trading hours, otherwise Tiingo
-    truncates the milliseconds to an iso compatible length"""
+    """Check if a crypto quote is less than 3 days old."""
     try:
         if time is not None:
             dtobj = parse(time)
-            tz_info = dtobj.tzinfo
-            if (datetime.now(tz_info) - dtobj) < timedelta(days=3):
+            if (datetime.now(dtobj.tzinfo) - dtobj) < timedelta(days=3):
                 return True
-            else:
-                return False
-        else:
-            return False
+        return False
     except Exception as e:
-        logger.error(f"testing time: {str(time)}, exception: {e}")
+        logger.error(f"Error testing time: {time}. Exception: {e}")
         return True
 
 
 def timezoner(stamp):
-    return parse(stamp).astimezone(tz.tzlocal()).strftime("%Y-%m-%d %H:%M %Z")
+    """Convert a timestamp to local time."""
+    return parse(stamp).astimezone(tz.UTC).strftime("%Y-%m-%d %H:%M %Z")
 
 
 def get_crypto(crypto):
-    """return a crypto quote, cleaned up"""
+    """Return a cleaned-up crypto quote."""
     TOKEN = os.environ["TIINGO_TOKEN"]
     validcrypto = validate_stonk(crypto)
     headers = {"Content-Type": "application/json"}
@@ -57,32 +47,18 @@ def get_crypto(crypto):
             f"https://api.tiingo.com/tiingo/crypto/top?tickers={validcrypto}usd&token={TOKEN}",
             headers=headers,
         )
-        validcrypto = response.json()
+        data = response.json()
     except Exception as e:
-        logger.error(f"Failed to connect to tiingo api. Reason: {e}")
-        validcrypto = []
-    # This returns a list of dictionaries with each item a crypto
-    # [{'askPrice': None, 'ticker': 'AAPL', 'mid': None, 'quoteTimestamp': '2021-03-15T20:00:00+00:00',
-    # 'timestamp': '2021-03-15T20:00:00+00:00', 'askSize': None, 'open': 121.41, 'prevClose': 121.03,
-    # 'tngoLast': 123.99, 'bidSize': None, 'lastSaleTimestamp': '2021-03-15T20:00:00+00:00',
-    # 'volume': 92590555, 'bidPrice': None, 'low': 120.42, 'lastSize': None, 'high': 124.0,
-    # 'last': 123.99}]
+        logger.error(f"Failed to connect to Tiingo API. Reason: {e}")
+        return {"Result": "Error connecting to Tiingo API."}
 
-    clean_crypto = {}
+    if not data:
+        logger.info(f"No results found for {crypto}.")
+        return {"Result": "Not found. Use a valid ticker."}
 
-    if validcrypto == []:
-        clean_crypto["Result"] = "Not Found, use a good ticker"
-        logger.info(f"ticker failed to return any results for {str(crypto)}")
-    else:
-        clean_crypto["Ticker"] = validcrypto[0]["ticker"]
-        clean_crypto["Quote Timestamp"] = timezoner(
-            validcrypto[0]["topOfBookData"][0]["quoteTimestamp"]
-        )
-        clean_crypto["Most Recent Price"] = validcrypto[0]["topOfBookData"][0][
-            "lastPrice"
-        ]
-        clean_crypto["Exchange Reporting"] = validcrypto[0]["topOfBookData"][0][
-            "lastExchange"
-        ]
-
-    return clean_crypto
+    return {
+        "Ticker": data[0]["ticker"],
+        "Quote Timestamp": timezoner(data[0]["topOfBookData"][0]["quoteTimestamp"]),
+        "Most Recent Price": data[0]["topOfBookData"][0]["lastPrice"],
+        "Exchange Reporting": data[0]["topOfBookData"][0]["lastExchange"],
+    }
---
+++ tests/test_tiingocrypto.py
@@ -0,0 +1,36 @@
+import os
+import requests
+import os
+import pytest
+import requests
+from tiingocrypto import get_crypto
+
+def test_get_crypto(mocker):
+    mocker.patch.dict(os.environ, {"TIINGO_TOKEN": "test_token"})
+    mock_response = [
+        {
+            "ticker": "BTC",
+            "topOfBookData": [
+                {
+                    "quoteTimestamp": "2021-03-15T20:00:00+00:00",
+                    "lastPrice": 60000.0,
+                    "lastExchange": "Coinbase",
+                }
+            ],
+        }
+    ]
+    mocker.patch.object(requests, "get", return_value=mocker.Mock(json=lambda: mock_response))
+    assert get_crypto("btcusd") == {
+        "Ticker": "BTC",
+        "Quote Timestamp": "2021-03-15 20:00 UTC",
+        "Most Recent Price": 60000.0,
+        "Exchange Reporting": "Coinbase",
+    }
+
+
+def test_get_crypto_invalid(mocker):
+    mocker.patch.dict(os.environ, {"TIINGO_TOKEN": "test_token"})
+    mock_response = []
+    mocker.patch.object(requests, "get", return_value=mocker.Mock(json=lambda: mock_response))
+    assert get_crypto("invalidticker") == {"Result": "Not found. Use a valid ticker."}
+    assert get_crypto(None) == {"Result": "Not found. Use a valid ticker."}
